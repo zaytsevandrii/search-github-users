521588bde67372ea026609255eb12250e939374a
Andrii Zaitsev <105815678+zaytsevandrii@users.noreply.github.com>
2022-12-09 09:30:45 +0000

    create styles form form

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  <>
+  <form className="user-form">
+    <input type="text" id='search' placeholder='Search a Github user' />
+  </form>
+  <main id="main">
+    <div className="card">
+      <div>
+        <img src="https://randomuser.me/api/portraits/men/30.jpg" className='avatar' alt="" />
+      </div>
+      <div className="user-info">
+        <h2>John Doe</h2>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sunt aspernatur nam explicabo, iure similique neque.</p>
+
+        <ul>
+          <li>300 <strong>Followers</strong></li>
+          <li>100 <strong>Following</strong></li>
+          <li>30 <strong>Repos</strong></li>
+        </ul>
+
+        <div id="repos">
+          <a href="#" className='repo'>Repo 1</a>
+          <a href="#" className='repo'>Repo 2</a>
+          <a href="#" className='repo'>Repo 3</a>
+        </div>
+      </div>
     </div>
+  </main>
+  </>
   );
 }
 
